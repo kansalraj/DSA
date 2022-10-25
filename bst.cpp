9bea6c399d74daa795d62549ffde82a51cc3a106
kansalraj <ankit.kansal1608@gmail.com>
2022-10-25 23:32:32 +0530

    BST To Balanced BST

--- bst.cpp
+++ bst.cpp
@@ -359,6 +359,24 @@ Node *flattenBSTToSortedLL(Node *root)
     return newRoot;
 }
 
+Node *inorderToBST(int s, int e, vector<int> &in)
+{
+    if (s > e)
+        return NULL;
+    int mid = (s + e) / 2;
+    Node *root = new Node(in[mid]);
+    root->left = inorderToBST(s, mid - 1, in);
+    root->right = inorderToBST(mid + 1, e, in);
+    return root;
+}
+
+Node *balancedBst(Node *root)
+{
+    vector<int> inorderVal;
+    storeUsingInorder(root, inorderVal);
+    return inorderToBST(0, inorderVal.size() - 1, inorderVal);
+}
+
 int main()
 {
     std::cout << "make BST" << std::endl;
