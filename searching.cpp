c8d3a5375511d2aade9a12f2502c2e375a1e2fc2
kansalraj <ankit.kansal1608@gmail.com>
2022-10-27 02:20:26 +0530

    First and Last Position of Element In Sorted Array

--- searching.cpp
+++ searching.cpp
@@ -37,4 +37,49 @@ int binarySearchOptimised(int *input, int n, int val)
     return -1;
 }
 
+int firstOccInSortedArray(vector<int> &input, int n, int val)
+{
+    int s = 0, e = n - 1, ans = -1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (input[mid] == val)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (input[mid] > val)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+int lastOccInSortedArray(vector<int> &input, int n, int val)
+{
+    int s = 0, e = n - 1, ans = -1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (input[mid] == val)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (input[mid] > val)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+{
+    return {firstOccInSortedArray(arr, n, k), lastOccInSortedArray(arr, n, k)};
+}
+
 int main() {}
