a5868858a93020467373d210aae27afe2ef7eb1b
kansalraj <ankit.kansal1608@gmail.com>
2022-10-27 02:01:00 +0530

    binary Search Optimised

--- searching.cpp
+++ searching.cpp
@@ -5,12 +5,34 @@ using namespace std;
 
 int binarySearch(int *input, int n, int val)
 {
-    int s = 0, e=n-1;
-    int mid=(s+e)/2;
-    while(s<=e) {
-        if (input[mid]==val) return mid;
-        if (input[mid] > val) e=mid-1; else s=mid+1;
-        mid=(s+e)/2;
+    int s = 0, e = n - 1;
+    int mid = (s + e) / 2;
+    while (s <= e)
+    {
+        if (input[mid] == val)
+            return mid;
+        if (input[mid] > val)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = (s + e) / 2;
+    }
+    return -1;
+}
+
+int binarySearchOptimised(int *input, int n, int val)
+{
+    int s = 0, e = n - 1;
+    int mid = s + (e - s) / 2;
+    while (s <= e)
+    {
+        if (input[mid] == val)
+            return mid;
+        if (input[mid] > val)
+            e = mid - 1;
+        else
+            s = mid + 1;
+        mid = s + (e - s) / 2;
     }
     return -1;
 }
