66271dcf17cffa9e0eeef4e06b3e9c4b55b75079
kansalraj <ankit.kansal1608@gmail.com>
2022-10-27 17:10:38 +0530

    find pivot in rotated sorted array

--- searching.cpp
+++ searching.cpp
@@ -114,4 +114,26 @@ int peakIndexInMountainArrayDifferentApproach(vector<int> &arr)
     }
     return s;
 }
-int main() {}
+
+int getPivot(int arr[], int n)
+{
+    int s = 0, e = n - 1;
+    int mid = (s + e) / 2;
+    while (s < e)
+    {
+        if (arr[mid] > arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+            e = mid;
+        mid = (s + e) / 2;
+    }
+    return s;
+}
+
+int main()
+{
+    int arr[5] = {3, 8, 10, 17, 1};
+    cout << getPivot(arr, 5);
+}
