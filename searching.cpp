d7be4b57f34d3116e3a9f384aa6153c7d216f5ab
kansalraj <ankit.kansal1608@gmail.com>
2022-10-31 22:45:08 +0530

    Insertion Sort

--- searching.cpp
+++ searching.cpp
@@ -324,6 +324,25 @@ void bubbleSort(vector<int> &arr, int n)
     }
 }
 
+void insertionSort(int n, vector<int> &arr)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+                break;
+        }
+        arr[j + 1] = temp;
+    }
+}
+
 int main()
 {
     // int arr[5] = {3, 8, 10, 17, 1};
