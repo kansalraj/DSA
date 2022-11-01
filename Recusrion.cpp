af745800050e746ef00f6a1e0ca2b0b0ee0ffce3
kansalraj <ankit.kansal1608@gmail.com>
2022-11-02 02:15:01 +0530

    1. factorial 2. fibonacci

---
+++ Recusrion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+#include <bits/stdc++.h>
+using namespace std;
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main() {}
